eab9cd16ef91ea8a0423872cb70d38bc44a7d314
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-08-09 10:48:13 -0300

    feat: signin function

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -25,6 +25,14 @@ export function SignIn() {
   const { signIn, isLoggingIn } = useAuth();
   const theme = useTheme();
 
+  async function handleSignIn() {
+    try {
+      await signIn()
+    } catch (error) {
+      Alert.alert('ERRO')
+    }
+  }
+
   // creates a function to handle sign in
     // try to call and wait signIn
     // if fails, display an Alert with the title "Erro SignIn" and message "Ocorreu um erro ao tentar logar no app"
@@ -61,19 +69,15 @@ export function SignIn() {
             o mundo da Twitch
           </Description>
 
-          {/* <SignInButton onPress={}>
+          <SignInButton onPress={handleSignIn}>
             <SignInButtonIcon>
-              Verify if isLoggingIn is true
-              If it is, show an ActivityIndicator
-              Otherwise, show Fontisto's twitch icon
+              {isLoggingIn ? <ActivityIndicator /> : <Fontisto name="twitch" color="white" />}
             </SignInButtonIcon>
 
             <SignInButtonText>
-              Verify if isLoggingIn is true
-              If it is, show "Entrando..."
-              Otherwise, show "Entrar com Twitch"
+              {isLoggingIn ? "Entrando..." : "Entrar com Twitch"}
             </SignInButtonText>
-          </SignInButton> */}
+          </SignInButton>
         </LoginInfo>
       </Content>
 
